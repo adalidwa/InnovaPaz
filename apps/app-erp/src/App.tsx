b85928b12bc922f1b14993a082488b743426b5ae
jesus ario soto mercado <jesusescar123@gmail.com>
2025-09-30 20:39:53 -0500

    feat(styles): component ui input updated

--- apps/app-erp/src/App.tsx
+++ apps/app-erp/src/App.tsx
@@ -2,9 +2,18 @@ import './App.css';
 import AppRoutes from './routes/AppRoutes';
 import Input from './components/common/Input';
 import { useState } from 'react';
+import Select from './components/common/Select';
 
 function App() {
   const [expiryDate, setExpiryDate] = useState('');
+  const [category, setCategory] = useState('');
+
+  const categories = [
+    { value: 'electronics', label: 'Electrónica' },
+    { value: 'clothing', label: 'Ropa' },
+    { value: 'food', label: 'Alimentos' },
+    { value: 'furniture', label: 'Muebles' },
+  ];
   return (
     <>
       <AppRoutes />
@@ -14,6 +23,13 @@ function App() {
         type='date'
         value={expiryDate}
         onChange={(e) => setExpiryDate(e.target.value)}
+      />
+      <Select
+        label='Categoría'
+        options={categories}
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        required
       />
     </>
   );
