a00ed46fd99726ed058357c50bc649501f153de6
jorian101 <whaam202101@gmail.com>
2025-09-30 22:43:27 -0400

    feat(erp): agregar sidebar

--- apps/app-erp/src/App.tsx
+++ apps/app-erp/src/App.tsx
@@ -1,36 +1,10 @@
 import './App.css';
 import AppRoutes from './routes/AppRoutes';
-import Input from './components/common/Input';
-import { useState } from 'react';
-import Select from './components/common/Select';
 
 function App() {
-  const [expiryDate, setExpiryDate] = useState('');
-  const [category, setCategory] = useState('');
-
-  const categories = [
-    { value: 'electronics', label: 'Electrónica' },
-    { value: 'clothing', label: 'Ropa' },
-    { value: 'food', label: 'Alimentos' },
-    { value: 'furniture', label: 'Muebles' },
-  ];
   return (
     <>
       <AppRoutes />
-      <Input label='Descripción' placeholder='Descripción del producto...' />
-      <Input
-        label='Válido Hasta'
-        type='date'
-        value={expiryDate}
-        onChange={(e) => setExpiryDate(e.target.value)}
-      />
-      <Select
-        label='Categoría'
-        options={categories}
-        value={category}
-        onChange={(e) => setCategory(e.target.value)}
-        required
-      />
     </>
   );
 }
