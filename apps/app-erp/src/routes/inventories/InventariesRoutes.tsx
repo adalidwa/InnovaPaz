049ad636c2d2b8b4a6a2be6f3d7159d4b2c578ad
jorian101 <whaam202101@gmail.com>
2025-10-01 00:13:01 -0400

    style(erp): agregar el uso del theme a summarycard

--- apps/app-erp/src/routes/inventories/InventariesRoutes.tsx
+++ apps/app-erp/src/routes/inventories/InventariesRoutes.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import ProductManagement from '../../features/inventories/pages/ProductManagement';
-
+import Dashboard from '../../features/inventories/pages/Dashboard';
 function InventariesRoutes() {
   return (
     <Routes>
       <Route index element={<ProductManagement />} />
+      <Route path='dashboard' element={<Dashboard />} />
     </Routes>
   );
 }
