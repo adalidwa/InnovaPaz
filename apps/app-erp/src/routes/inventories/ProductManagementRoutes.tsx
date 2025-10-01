6531fe8440b9d7bba9d847ebc3c54ca2a0b96b1e
jorian101 <whaam202101@gmail.com>
2025-10-01 09:17:19 -0400

    feat(erp): quitar doble ruta de dashboard

--- apps/app-erp/src/routes/inventories/ProductManagementRoutes.tsx
+++ apps/app-erp/src/routes/inventories/ProductManagementRoutes.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { ProductsProvider } from '../../features/inventories/context/ProductsContext';
 import ProductManagement from '../../features/inventories/pages/ProductManagement';
-import Dashboard from '../../features/inventories/pages/Dashboard';
 import SeeInventories from '../../features/inventories/pages/SeeInventories';
 
 function ProductManagementRoutes() {
@@ -9,7 +8,6 @@ function ProductManagementRoutes() {
     <ProductsProvider>
       <Routes>
         <Route index element={<ProductManagement />} />
-        <Route path='dashboard' element={<Dashboard />} />
         <Route path='ver' element={<SeeInventories />} />
         <Route path='ver/:id' element={<SeeInventories />} />
       </Routes>
