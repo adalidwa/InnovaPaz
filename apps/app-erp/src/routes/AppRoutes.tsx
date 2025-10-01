37bbe061116df8dba8fe097ce4b4cab2aef9c3eb
edisonCheca <echecat@est.emi.edu.bo>
2025-10-01 08:43:24 -0400

    merge(erp): sincronizacion con la rama remota con la local

--- apps/app-erp/src/routes/AppRoutes.tsx
+++ apps/app-erp/src/routes/AppRoutes.tsx
@@ -3,13 +3,17 @@ import ShoppingRoutes from './shopping/ShoppingRoutes';
 import ProductManagementRoutes from './inventories/ProductManagementRoutes';
 import ReportsRoutes from './reports/ReportsRoutes';
 import UserRoutes from './users/UserRoutes';
+import SalesRoutes from './sales/SalesRoutes';
+import Dashboard from '../features/inventories/pages/Dashboard';
 
 const AppRoutes = () => (
   <Routes>
+    <Route path='/*' element={<SalesRoutes />} />
     <Route path='/shopping/*' element={<ShoppingRoutes />} />
     <Route path='/productos/*' element={<ProductManagementRoutes />} />
     <Route path='/reportes/*' element={<ReportsRoutes />} />
     <Route path='/configuracion/*' element={<UserRoutes />} />
+    <Route path='/dashboard/*' element={<Dashboard />} />
   </Routes>
 );
 
