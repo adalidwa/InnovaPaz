d60f15022cb4b4358b5fd0e37e2e40c9802689ff
adalidwa <adalidjesusxd@gmail.com>
2025-10-10 21:15:59 -0400

    feat(api): creando el backend de inventarios

--- apps/app-erp-backend/index.js
+++ apps/app-erp-backend/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const taskRoutes = require('./routes/task.routes');
+const inventoryRoutes = require('./routes/inventories.routes');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use(taskRoutes);
+app.use('/api', inventoryRoutes);
 
 app.use((err, req, res, next) => {
   return res.json({
